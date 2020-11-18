8b94908fabd2741393fd1c20479034c7b53b90f8
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-18 23:41:33 +0300

    First commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
     return (
         <View>
-            <StatusBar style="auto" />
+            <StatusBar style="autoa" />
         </View>
     );
 }
