192f9088007c0b73a471c49413bd9faab6aa38de
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-19 03:16:21 +0300

    Test screens

---
+++ src/screens/test1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const Test1 = () => {
+    return (
+        <View style={{padding: 10}}>
+            <Text>Test1111111111111111</Text>
+        </View>
+    )
+}
---
+++ src/screens/test2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+
+export const Test2 = () => {
+    return (
+        <View>
+            <Text>Test2222222222222</Text>
+        </View>
+    )
+}
