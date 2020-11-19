bef0356a1d03c3c9ef5f2f4be6639ca5cacb9027
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-20 02:41:59 +0300

    Styles for menu

---
+++ src/styles/Routes.js
@@ -0,0 +1,11 @@
+export const drawerStyles = {
+    activeTintColor: 'black',
+    activeBackgroundColor: 'white',
+    inactiveTintColor: 'black',
+    inactiveBackgroundColor: 'white',
+    justifyContent: 'center',
+};
+
+export const screenOptionStyle = {
+    headerTitleAlign: "center"
+};
