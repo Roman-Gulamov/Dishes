ce89eb3fb8ec56997c441c8c4819f8251722dae1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-20 02:42:51 +0300

    Styles for the button

---
+++ src/styles/NavButtonWrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const NavButtonWrapper = styled.TouchableOpacity`
+    position: absolute;
+    top: -7%;
+    left: 5%;
+    z-index: 999;
+`;
