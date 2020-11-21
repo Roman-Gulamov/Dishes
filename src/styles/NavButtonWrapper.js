f5885c1bfd6de13ff91d0834580cb21b168dcccc
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 03:45:28 +0300

    Removed unnecessary files

--- src/styles/NavButtonWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const NavButtonWrapper = styled.TouchableOpacity`
-    position: absolute;
-    top: -7%;
-    left: 5%;
-    z-index: 999;
-`;
