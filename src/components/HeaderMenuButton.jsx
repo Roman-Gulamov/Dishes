e88979e682bf8da05502cebf7f781e276f5a6200
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 23:23:24 +0300

    Renamed menu component

---
+++ src/components/HeaderMenuButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+
+export const HeaderMenuButton = ({ onPress }) => {
+    return (
+        <TouchableOpacity 
+            activeOpacity={0.4} 
+            onPress={onPress} 
+            style={{paddingLeft: 10}}
+        >
+            <MaterialIcons
+                name='menu'
+                size={32}
+                color='black'
+            />
+        </TouchableOpacity>
+    )
+}
