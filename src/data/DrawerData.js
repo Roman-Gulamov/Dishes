a528185dc86ede4bbd77d246a7ce6efcf2d6445c
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 03:46:18 +0300

    Data file

---
+++ src/data/DrawerData.js
@@ -0,0 +1,17 @@
+export const DRAWER_DATA = [
+    {
+        id: '1',
+        name: 'Home',
+        iconName: 'home'
+    },
+    {
+        id: '2',
+        name: 'Categories',
+        iconName: 'book-open'
+    },
+    {
+        id: '3',
+        name: 'Search',
+        iconName: 'search'
+    },
+]
