e8b58ff527b9ba13d2e595fbd551beb70f16cbcc
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 03:48:23 +0300

    Rewritten routing component

--- src/routes/StackRoute.jsx
+++ src/routes/StackRoute.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import { HeaderBackButton } from '@react-navigation/stack';
 
 import { screenOptionStyle } from '../styles/Routes';
 
+import { NavButton } from '../components/NavButton';
 import { Home } from '../screens/Home';
 import { Categories } from '../screens/Categories';
 import { Search } from '../screens/Search';
 
-const Stack = createStackNavigator();
 
 export const MainRoute = ({ screenName }) => {
+    const navigation = useNavigation();
+    const Stack = createStackNavigator();
+
     return (
-        <Stack.Navigator screenOptions={screenOptionStyle}> 
-            <Stack.Screen 
-                name={screenName} 
-                component={
-                    screenName === 'Home' ? Home : 
-                    screenName === 'Categories' ? Categories : 
-                    Search
-                }
-            />
-        </Stack.Navigator>
+        <>
+            <Stack.Navigator screenOptions={screenOptionStyle} >   
+                <Stack.Screen 
+                    name={screenName} 
+                    component={
+                        screenName === 'Home' ? Home : 
+                        screenName === 'Categories' ? Categories : Search
+                    }
+                    options={{
+                        headerLeft: () => (
+                        screenName !== 'Home' ? 
+                            <HeaderBackButton onPress={() => navigation.goBack()} /> : 
+                            <NavButton onPress={() => navigation.toggleDrawer()} />
+                        ),
+                    }}
+                />
+            </Stack.Navigator>
+        </>
     )
 }
 
