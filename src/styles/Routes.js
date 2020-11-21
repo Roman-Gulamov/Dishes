d851b0cecc25bb9d3c867109139a0e9f8d39cc78
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 03:47:05 +0300

    Updated styles for navigation

--- src/styles/Routes.js
+++ src/styles/Routes.js
@@ -1,11 +1,20 @@
+import { Dimensions } from 'react-native';
+
+const windowHeight = Dimensions.get('window').height;
+
 export const drawerStyles = {
     activeTintColor: 'black',
     activeBackgroundColor: 'white',
     inactiveTintColor: 'black',
     inactiveBackgroundColor: 'white',
-    justifyContent: 'center',
+    justifyContent: 'center'
 };
 
 export const screenOptionStyle = {
-    headerTitleAlign: "center"
+    headerTitleAlign: "center",
+    headerStyle: {
+        height: windowHeight / 10 
+    }
 };
+
+//! height: windowHeight / 10  посмотри как без него
