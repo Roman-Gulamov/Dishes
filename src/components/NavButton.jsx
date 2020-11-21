7a03e1f57f55d05734ce70c6604e6c10f3928b6b
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-21 03:47:45 +0300

    Changed markup

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-import { NavButtonWrapper } from '../styles/NavButtonWrapper';
 
-
-export const NavButton = ({ iconName, onPress }) => {
+export const NavButton = ({ onPress }) => {
     return (
-        <NavButtonWrapper activeOpacity={0.4} onPress={onPress}>
+        <TouchableOpacity 
+            activeOpacity={0.4} 
+            onPress={onPress} 
+            style={{paddingLeft: 10}}
+        >
             <MaterialIcons
-                name={iconName}
+                name='menu'
                 size={32}
                 color='black'
             />
-        </NavButtonWrapper>
+        </TouchableOpacity>
     )
 }
