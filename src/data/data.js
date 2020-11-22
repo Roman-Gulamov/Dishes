3d3ed1a4a15b214c504f15cd8ea03fa0471a439b
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-22 22:37:57 +0300

    Data updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,122 +1,128 @@
 export const DRAWER_DATA = [
     {
-        id: '1',
-        name: 'Home',
-        iconName: 'home'
+        id: "1",
+        name: "Home",
+        iconName: "home"
     },
     {
-        id: '2',
-        name: 'Categories',
-        iconName: 'book-open'
+        id: "2",
+        name: "Categories",
+        iconName: "book-open"
     },
     {
-        id: '3',
-        name: 'Search',
-        iconName: 'search'
+        id: "3",
+        name: "Search",
+        iconName: "search"
     },
 ];
 
 
 export const CATEGORIES_DATA = [
     {
-        id: "1",
-        name: 'Cookies',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Cookies_w4kpgr.jpg',
-        recipeCount: '4 recipes'
+        dishesId: "1",
+        name: "cookies",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Cookies_w4kpgr.jpg",
     },
     {
-        id: "2",
-        name: 'Mexican Food',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/MexicanFood_u8fcok.jpg',
-        recipeCount: '4 recipes'
+        dishesId: "2",
+        name: "mexican food",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/MexicanFood_u8fcok.jpg",
     },
     {
-        id: "3",
-        name: 'Italian Food',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605989698/Dishes/Categories/ItalianFood_atulb9.jpg',
-        recipeCount: '4 recipes'
+        dishesId: "3",
+        name: "italian food",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605989698/Dishes/Categories/ItalianFood_atulb9.jpg",
     },
     {
-        id: "4",
-        name: 'Smoothies',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Smoothies_v8psad.jpg',
-        recipeCount: '4 recipes'
+        dishesId: "4",
+        name: "smoothies",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Smoothies_v8psad.jpg",
     },
     {
-        id: "5",
-        name: 'Pizza',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Pizza_sovzwd.jpg',
-        recipeCount: '4 recipes'
+        dishesId: "5",
+        name: "pizza",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605989697/Dishes/Categories/Pizza_sovzwd.jpg",
     }
 ];
 
 
 export const DISHES_DATA = [
     {
-        id: '1',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976104/Dishes/Home/OatmealCookie_wgnivk.jpg',
-        count: '4 recipes',
-        title: 'oatmeal cookies',
-        categories: 'cookies'
+        id: "1",
+        dishesId: "1",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976104/Dishes/Home/OatmealCookie_wgnivk.jpg",
+        count: "4 recipes",
+        title: "oatmeal cookies",
+        categories: "cookies"
     },
     {
-        id: '2',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/MixedBerrySmoothie_m7ffec.jpg',
-        title: 'triple berry smoothie',
-        categories: 'smoothies'
+        id: "2",
+        dishesId: "4",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/MixedBerrySmoothie_m7ffec.jpg",
+        title: "triple berry smoothie",
+        categories: "smoothies"
     },
     {
-        id: '3',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/VeganCookies_gqcsf2.jpg',
-        title: 'vegan cookies',
-        categories: 'cookies'
+        id: "3",
+        dishesId: "1",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/VeganCookies_gqcsf2.jpg",
+        title: "vegan cookies",
+        categories: "cookies"
     },
     {
-        id: '4',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/PumpkinCookies_jh5rt5.jpg',
-        title: 'pumpkin spice cookies',
-        categories: 'cookies'
+        id: "4",
+        dishesId: "1",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/PumpkinCookies_jh5rt5.jpg",
+        title: "pumpkin spice cookies",
+        categories: "cookies"
     },
     {
-        id: '5',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/Brownies_bpsflt.jpg',
-        title: 'brownies',
-        categories: 'cookies'
+        id: "5",
+        dishesId: "1",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/Brownies_bpsflt.jpg",
+        title: "brownies",
+        categories: "cookies"
     },
     {
-        id: '6',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/FishTacos_kz1vg3.jpg',
-        title: 'perfect fish tacos',
-        categories: 'mexican food'
+        id: "6",
+        dishesId: "2",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/FishTacos_kz1vg3.jpg",
+        title: "perfect fish tacos",
+        categories: "mexican food"
     },
     {
-        id: '7',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/ChickenFajitas_zcedwj.jpg',
-        title: 'chicken fajitas',
-        categories: 'mexican food'
+        id: "7",
+        dishesId: "2",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/ChickenFajitas_zcedwj.jpg",
+        title: "chicken fajitas",
+        categories: "mexican food"
     },
     {
-        id: '8',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/BuffaloPizza_uemkqi.jpg',
-        title: 'buffalo pizza',
-        categories: 'italian food'
+        id: "8",
+        dishesId: "3",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/BuffaloPizza_uemkqi.jpg",
+        title: "buffalo pizza",
+        categories: "italian food"
     },
     {
-        id: '9',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/BestLasagna_ajouzv.jpg',
-        title: 'classic lasagna',
-        categories: 'pizza'
+        id: "9",
+        dishesId: "5",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976745/Dishes/Home/BestLasagna_ajouzv.jpg",
+        title: "classic lasagna",
+        categories: "pizza"
     },
     {
-        id: '10',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/SpaghettiCarbonara_mco6mr.jpg',
-        title: 'spaghetti carbonara',
-        categories: 'italian food'
+        id: "10",
+        dishesId: "3",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/SpaghettiCarbonara_mco6mr.jpg",
+        title: "spaghetti carbonara",
+        categories: "italian food"
     },
     {
-        id: '11',
-        image: 'https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/Lasagna_sygklu.jpg',
-        title: 'lazania',
-        categories: 'italian food'
+        id: "11",
+        dishesId: "3",
+        image: "https://res.cloudinary.com/romangulamov/image/upload/v1605976746/Dishes/Home/Lasagna_sygklu.jpg",
+        title: "lazania",
+        categories: "italian food"
     }
 ];
