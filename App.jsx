7965bb60f4fbd049f70533e9d839e9ffc5c3609d
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-22 22:37:40 +0300

    Added context and states

--- App.jsx
+++ App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import SearchContext from './src/services/context';
 import { DrawerNavigator } from './src/routes/DrawerNavigator';
 
-
 const App = () => {
+    const [searchValue, setSearchValue] = useState('');
+
     return (
-        <NavigationContainer>
-            <DrawerNavigator />
-        </NavigationContainer>
+        <SearchContext.Provider value={{searchValue, setSearchValue}}>
+            <NavigationContainer>
+                <DrawerNavigator />
+            </NavigationContainer>
+        </SearchContext.Provider>
     )
 }
 
