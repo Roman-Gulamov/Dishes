3e82c50377791236afe18f4748af7b8f22cab561
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-22 22:36:17 +0300

    Styles for SearchBar

---
+++ src/styles/Search.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    containerStyle: {
+        flexDirection: "row",
+        backgroundColor: 'transparent',
+        borderTopColor: 'transparent',
+        borderBottomColor: 'transparent'
+    },
+
+    inputContainerStyle: {
+        backgroundColor: 'rgba(199,199,199,0.2)', 
+        borderRadius: 15
+    },
+
+    inputStyle: {
+        color: 'rgba(0, 0, 0, 0.7)'
+    }
+});
