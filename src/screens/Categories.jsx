f3c7b23fb7ace0277b70a4f624bb4ce000eaa53e
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-22 22:38:52 +0300

    Category page updated

--- src/screens/Categories.jsx
+++ src/screens/Categories.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 
-import { CATEGORIES_DATA } from '../data/data';
+import { getDishesCount } from '../components/getDishesCount';
 import { FlatListPacker } from '../components/FlatListPacker';
 import { CardMap } from '../components/CardMap';
 
+import { CATEGORIES_DATA } from '../data/data';
+
 import { styles } from '../styles/Cards';
+
 
 
 export const Categories = () => {
     return (
         <FlatListPacker 
             data={CATEGORIES_DATA}
+            numColumns={1}
+            keyExtractor={item => item.id}
+            contentContainerStyle={styles.categoriesContent}
+            cardStyle={styles.categoriesCard}
             renderItem={({ item }) => 
                 <CardMap 
                     image={item.image} 
                     title={item.name} 
-                    subtitle={item.recipeCount} 
+                    subtitle={`${getDishesCount(item.dishesId)} recipes`}
                     imageStyle={styles.categoriesImage}
                     cardStyle={styles.categoriesCard} 
                     titleStyle={styles.categoriesTitle}
                     subtitleStyle={styles.categoriesSubtitle}
                 />
             }
-            numColumns={1}
-            contentContainerStyle={styles.categoriesContent}
-            cardStyle={styles.categoriesCard}
         />
     )
 }
